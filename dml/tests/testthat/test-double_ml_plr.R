f9a0adbe38fa42c10cf8a3c0ed89d6a5c18efab7
malte <malte@scalable.capital>
2019-10-01 15:28:47 +0200

    also test standard errors

--- dml/tests/testthat/test-double_ml_plr.R
+++ dml/tests/testthat/test-double_ml_plr.R
@@ -17,6 +17,7 @@ patrick::with_parameters_test_that("Regression tests for dlm estimates for parti
   set.seed(i_setting)
   
   all_thetas <- vector('numeric', length=n_settings)
+  all_ses <- vector('numeric', length=n_settings)
   for (i_setting in 1:n_settings) {
     set.seed(1234)
     cf <- mlr3::rsmp("cv", folds = 5)
@@ -24,11 +25,14 @@ patrick::with_parameters_test_that("Regression tests for dlm estimates for parti
                        resampling = cf, mlmethod = learner_pars$mlmethod,
                        params = list(params_m = learner_pars$params$params_m,
                          params_g =  learner_pars$params$params_g),
-                       dml_procedure = dml_procedure, inf_model = inf_model)
+                       dml_procedure = dml_procedure, inf_model = inf_model,
+                       se_type = inf_model)
     all_thetas[i_setting] <- coef(plr_hat)
+    all_ses[i_setting] <- plr_hat$se
   }
   
   all_thetas_obj <- vector('numeric', length=n_settings)
+  all_ses_obj <- vector('numeric', length=n_settings)
   for (i_setting in 1:n_settings) {
     set.seed(1234)
     double_mlplr_obj = DoubleMLPLR$new(n_folds = 5,
@@ -38,9 +42,11 @@ patrick::with_parameters_test_that("Regression tests for dlm estimates for parti
                                        dml_procedure = dml_procedure, inf_model = inf_model)
     double_mlplr_obj$fit(data_plm[[i_setting]], y = "y", d = "Var1")
     all_thetas_obj[i_setting] <- double_mlplr_obj$coef
+    all_ses_obj[i_setting] <- double_mlplr_obj$se
   }
   
-  expect_equal(all_thetas, all_thetas_obj, tolerance = 1e-4)
+  expect_equal(all_thetas, all_thetas_obj, tolerance = 1e-8)
+  expect_equal(all_ses, all_ses_obj, tolerance = 1e-8)
 }
 )
 
