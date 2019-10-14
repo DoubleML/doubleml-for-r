cc1bad3baeb2eb6909888983b2b24725d15799d2
malte <malte@scalable.capital>
2019-10-14 10:46:29 +0200

    updates for iivm model

--- dml/R/double_ml_iivm.R
+++ dml/R/double_ml_iivm.R
@@ -29,125 +29,125 @@ private = list(
     # get conditional samples (conditioned on z = 0 or z = 1)
     private$get_cond_smpls(data[ , z])
     
-    # nuisance m
-    m_indx <- names(data) != d & names(data) != z
-    data_m <- data[, m_indx, drop = FALSE]
-    data_m[, z] <- factor(data_m[, z])
+    # nuisance p
+    p_indx <- names(data) != y & names(data) != d
+    data_p <- data[, p_indx, drop = FALSE]
+    data_p[, z] <- factor(data_p[, z])
     
-    task_m <- mlr3::TaskClassif$new(id = paste0("nuis_p_", z), backend = data_m,
+    task_p <- mlr3::TaskClassif$new(id = paste0("nuis_p_", z), backend = data_p,
                                     target = z, positive = "1")
-    ml_m <- mlr3::lrn(self$ml_learners$mlmethod_m, predict_type = "prob")
-    ml_m$param_set$values <- self$params$params_m # tbd: check if parameter passing really works
+    ml_p <- mlr3::lrn(self$ml_learners$mlmethod_p, predict_type = "prob")
+    ml_p$param_set$values <- self$params$params_p # tbd: check if parameter passing really works
     
     # instantiate custom resampling with already sampled train and test ids
-    resampling_m <- mlr3::rsmp("custom")
-    resampling_m$instantiate(task_m,
+    resampling_p <- mlr3::rsmp("custom")
+    resampling_p$instantiate(task_p,
                              private$smpls$train_ids,
                              private$smpls$test_ids)
     
-    r_m <- mlr3::resample(task_m, ml_m, resampling_m, store_models = TRUE)
+    r_p <- mlr3::resample(task_p, ml_p, resampling_p, store_models = TRUE)
     
-    m_hat <- as.data.table(r_m$prediction())
-    setorder(m_hat, 'row_id')
-    m_hat <- m_hat$prob.1
+    p_hat <- as.data.table(r_p$prediction())
+    setorder(p_hat, 'row_id')
+    p_hat <- p_hat$prob.1
     
     
-    # nuisance g
-    g_indx <- names(data) != d & names(data) != z
-    data_g <- data[ , g_indx, drop = FALSE]
+    # nuisance mu
+    mu_indx <- names(data) != d & names(data) != z
+    data_mu <- data[ , mu_indx, drop = FALSE]
     
-    # g0
-    task_g0 <- mlr3::TaskRegr$new(id = paste0("nuis_g0_", y), backend = data_g, target = y)
+    # mu0
+    task_mu0 <- mlr3::TaskRegr$new(id = paste0("nuis_mu0_", y), backend = data_mu, target = y)
     
     # instantiate custom resampling with already sampled train and test ids
-    resampling_g0 <- mlr3::rsmp("custom")
-    resampling_g0$instantiate(task_g0,
+    resampling_mu0 <- mlr3::rsmp("custom")
+    resampling_mu0$instantiate(task_mu0,
                               private$smpls$train_ids_0,
                               private$smpls$test_ids)
     
-    ml_g0 <- mlr3::lrn(self$ml_learners$mlmethod_g0)
-    ml_g0$param_set$values <- self$params$params_g0
+    ml_mu0 <- mlr3::lrn(self$ml_learners$mlmethod_mu0)
+    ml_mu0$param_set$values <- self$params$params_mu0
     
-    r_g0 <- mlr3::resample(task_g0, ml_g0, resampling_g0, store_models = TRUE)
+    r_mu0 <- mlr3::resample(task_mu0, ml_mu0, resampling_mu0, store_models = TRUE)
     
-    g0_hat = as.data.table(r_g0$prediction())
-    setorder(g0_hat, 'row_id')
-    g0_hat = g0_hat$response
+    mu0_hat = as.data.table(r_mu0$prediction())
+    setorder(mu0_hat, 'row_id')
+    mu0_hat = mu0_hat$response
     
-    # g1
-    task_g1 <- mlr3::TaskRegr$new(id = paste0("nuis_g1_", y), backend = data_g, target = y)
+    # mu1
+    task_mu1 <- mlr3::TaskRegr$new(id = paste0("nuis_mu1_", y), backend = data_mu, target = y)
     
     # instantiate custom resampling with already sampled train and test ids
-    resampling_g1 <- mlr3::rsmp("custom")
-    resampling_g1$instantiate(task_g1,
+    resampling_mu1 <- mlr3::rsmp("custom")
+    resampling_mu1$instantiate(task_mu1,
                               private$smpls$train_ids_1,
                               private$smpls$test_ids)
     
-    ml_g1 <- mlr3::lrn(self$ml_learners$mlmethod_g1)
-    ml_g1$param_set$values <- self$params$params_g1
+    ml_mu1 <- mlr3::lrn(self$ml_learners$mlmethod_mu1)
+    ml_mu1$param_set$values <- self$params$params_mu1
     
-    r_g1 <- mlr3::resample(task_g1, ml_g1, resampling_g1, store_models = TRUE)
+    r_mu1 <- mlr3::resample(task_mu1, ml_mu1, resampling_mu1, store_models = TRUE)
     
-    g1_hat = as.data.table(r_g1$prediction())
-    setorder(g1_hat, 'row_id')
-    g1_hat = g1_hat$response
+    mu1_hat = as.data.table(r_mu1$prediction())
+    setorder(mu1_hat, 'row_id')
+    mu1_hat = mu1_hat$response
     
-    # nuisance r
-    r_indx <- names(data) != d & names(data) != z
-    data_r <- data[ , r_indx, drop = FALSE]
-    data_r[, d] <- factor(data_r[, d])
+    # nuisance m
+    m_indx <- names(data) != y & names(data) != z
+    data_m <- data[ , m_indx, drop = FALSE]
+    data_m[, d] <- factor(data_m[, d])
     
-    # r0
-    task_r0 <- mlr3::TaskClassif$new(id = paste0("nuis_r0_", d), backend = data_r,
+    # m0
+    task_m0 <- mlr3::TaskClassif$new(id = paste0("nuis_m0_", d), backend = data_m,
                                      target = d, positive = "1")
     
     # instantiate custom resampling with already sampled train and test ids
-    resampling_r0 <- mlr3::rsmp("custom")
-    resampling_r0$instantiate(task_r0,
+    resampling_m0 <- mlr3::rsmp("custom")
+    resampling_m0$instantiate(task_m0,
                               private$smpls$train_ids_0,
                               private$smpls$test_ids)
     
-    ml_r0 <- mlr3::lrn(self$ml_learners$mlmethod_r0, predict_type = "prob")
-    ml_r0$param_set$values <- self$params$params_r0
+    ml_m0 <- mlr3::lrn(self$ml_learners$mlmethod_m0, predict_type = "prob")
+    ml_m0$param_set$values <- self$params$params_m0
     
-    r_r0 <- mlr3::resample(task_r0, ml_r0, resampling_r0, store_models = TRUE)
+    r_m0 <- mlr3::resample(task_m0, ml_m0, resampling_m0, store_models = TRUE)
     
-    r0_hat = as.data.table(r_r0$prediction())
-    setorder(r0_hat, 'row_id')
-    r0_hat <- r0_hat$prob.1
+    m0_hat = as.data.table(r_m0$prediction())
+    setorder(m0_hat, 'row_id')
+    m0_hat <- m0_hat$prob.1
     
-    # r1
-    task_r1 <- mlr3::TaskClassif$new(id = paste0("nuis_r1_", d), backend = data_r,
+    # m1
+    task_m1 <- mlr3::TaskClassif$new(id = paste0("nuis_m1_", d), backend = data_m,
                                      target = d, positive = "1")
     
     # instantiate custom resampling with already sampled train and test ids
-    resampling_r1 <- mlr3::rsmp("custom")
-    resampling_r1$instantiate(task_r1,
+    resampling_m1 <- mlr3::rsmp("custom")
+    resampling_m1$instantiate(task_m1,
                               private$smpls$train_ids_1,
                               private$smpls$test_ids)
     
-    ml_r1 <- mlr3::lrn(self$ml_learners$mlmethod_r1, predict_type = "prob")
-    ml_r1$param_set$values <- self$params$params_r1
+    ml_m1 <- mlr3::lrn(self$ml_learners$mlmethod_m1, predict_type = "prob")
+    ml_m1$param_set$values <- self$params$params_m1
     
-    r_r1 <- mlr3::resample(task_r1, ml_r1, resampling_r1, store_models = TRUE)
+    r_m1 <- mlr3::resample(task_m1, ml_m1, resampling_m1, store_models = TRUE)
     
-    r1_hat = as.data.table(r_r1$prediction())
-    setorder(r1_hat, 'row_id')
-    r1_hat <- r1_hat$prob.1
+    m1_hat = as.data.table(r_m1$prediction())
+    setorder(m1_hat, 'row_id')
+    m1_hat <- m1_hat$prob.1
     
     
     # compute residuals
     Z <- data[ , z]
     D <- data[ , d]
     Y <- data[ , y]
-    u_hat1 = Y - g_hat1
-    w_hat0 = D - r_hat0
-    w_hat1 = D - r_hat1
+    u1_hat = Y - mu1_hat
+    w0_hat = D - m0_hat
+    w1_hat = D - m1_hat
     
     
     if (self$inf_model == 'LATE') {
-      private$score_b = g1_hat - g0_hat + Z*(u1_hat)/m_hat - (1-Z)*u1_hat/(1-m_hat)
-      private$score_a = -1 * (r1_hat - r0_hat + Z*(w1_hat)/m_hat - (1-Z)*w0_hat/(1-m_hat))
+      private$score_b = mu1_hat - mu0_hat + Z*(u1_hat)/p_hat - (1-Z)*u1_hat/(1-p_hat)
+      private$score_a = -1 * (m1_hat - m0_hat + Z*(w1_hat)/p_hat - (1-Z)*w0_hat/(1-p_hat))
     }
     
     invisible(self)
