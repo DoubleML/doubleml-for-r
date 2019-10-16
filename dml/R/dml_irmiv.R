7c08f4e3ae656c0c75fa442dcaadf06b33601b8f
malte <malte@scalable.capital>
2019-10-16 15:50:11 +0200

    indentation

--- dml/R/dml_irmiv.R
+++ dml/R/dml_irmiv.R
@@ -416,13 +416,13 @@ var_irmiv <-  function(theta, p_hat, mu0_hat, mu1_hat, m0_hat, m1_hat, d, y, z, 
   var <- NA
 
   if (inf_model == "LATE") {
-  
-  var <- mean(1/length(d) * 1/(colMeans( m1_hat - m0_hat + z*(d-m1_hat)/p_hat 
-                                - (1-z)*(d-m0_hat)/(1-p_hat) , na.rm = TRUE))^2 * 
-              colMeans( ( mu1_hat - mu0_hat + z*(y - mu1_hat)/p_hat - 
-                        (1-z)*(y - mu0_hat)/(1-p_hat))^2 , na.rm = TRUE) )
-
-     } 
+    
+     var <- mean(1/length(d) * 1/(colMeans( m1_hat - m0_hat + z*(d-m1_hat)/p_hat 
+                                  - (1-z)*(d-m0_hat)/(1-p_hat) , na.rm = TRUE))^2 * 
+                colMeans( ( mu1_hat - mu0_hat + z*(y - mu1_hat)/p_hat - 
+                            (1-z)*(y - mu0_hat)/(1-p_hat))^2 , na.rm = TRUE) )
+    
+  } 
   
   # else if (inf_model == "DML2018") {
   # 
