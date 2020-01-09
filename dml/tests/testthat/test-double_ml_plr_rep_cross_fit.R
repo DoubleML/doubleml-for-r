4b06e65b27f8d252a595e29ba20363f0c5f74207
malte <malte@scalable.capital>
2020-01-09 17:11:12 +0100

    for alignment of sample splitting, we run without bootstrap

--- dml/tests/testthat/test-double_ml_plr_rep_cross_fit.R
+++ dml/tests/testthat/test-double_ml_plr_rep_cross_fit.R
@@ -18,20 +18,22 @@ patrick::with_parameters_test_that("Unit tests for PLR:",
                                    .cases = test_cases, {
   
   learner_pars <- get_default_mlmethod_plr(learner)
+  learner_pars_for_DML <- learner_pars
+  learner_pars_for_DML$params$params_g = rep(list(learner_pars_for_DML$params$params_g), 1)
+  learner_pars_for_DML$params$params_m = rep(list(learner_pars_for_DML$params$params_m), 1)
   
   set.seed(i_setting)
   n_folds = 5
   plr_hat <- DML(data_plr[[i_setting]], y = "y", d = "d",
                  model = "plr",
                  k = n_folds, S = n_rep_cross_fit,
-                 mlmethod = learner_pars$mlmethod,
-                 params = learner_pars$params,
+                 mlmethod = learner_pars_for_DML$mlmethod,
+                 params = learner_pars_for_DML$params,
                  dml_procedure = dml_procedure, inf_model = inf_model,
                  se_type = inf_model,
-                 bootstrap = "normal", nRep = 500)
+                 bootstrap = "none", nRep = 500)
   theta <- coef(plr_hat)
   se <- plr_hat$se
-  
   
   set.seed(i_setting)
   double_mlplr_obj = DoubleMLPLR$new(n_folds = n_folds,
@@ -43,17 +45,9 @@ patrick::with_parameters_test_that("Unit tests for PLR:",
   theta_obj <- double_mlplr_obj$coef
   se_obj <- double_mlplr_obj$se
   
-  # bootstrap
-  double_mlplr_obj$bootstrap(method = 'normal',  n_rep = 500)
-  boot_theta_obj = double_mlplr_obj$boot_coef
-  
-  
   # at the moment the object result comes without a name
   expect_equal(theta, c(d=theta_obj), tolerance = 1e-8)
   expect_equal(se, c(d=se_obj), tolerance = 1e-8)
-  if (dml_procedure == "dml2") {
-    expect_equal(as.vector(plr_hat$boot_theta), as.vector(boot_theta_obj), tolerance = 1e-8)
-  }
 }
 )
 
