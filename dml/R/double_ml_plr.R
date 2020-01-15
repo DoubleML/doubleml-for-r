86083ce41b50be29cb0cbc7f0c413f62e748c972
Philipp Bach <bach.philipp@outlook.com>
2020-01-16 00:07:05 +0100

    Initialization of PLR class for se_reestimate

--- dml/R/double_ml_plr.R
+++ dml/R/double_ml_plr.R
@@ -10,12 +10,14 @@ DoubleMLPLR <- R6Class("DoubleMLPLR", inherit = DoubleML, public = list(
                         params = list(params_m = list(),
                                       params_g = list()),
                         dml_procedure,
+                        se_reestimate,
                         inf_model,
                         n_rep_cross_fit=1) {
     super$initialize_double_ml(n_folds,
                                ml_learners,
                                params,
                                dml_procedure,
+                               se_reestimate,
                                inf_model,
                                n_rep_cross_fit)
   }
