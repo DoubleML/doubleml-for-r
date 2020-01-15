64ea14b46cf00d2f04a8abf493cbefe62defa812
Philipp Bach <bach.philipp@outlook.com>
2020-01-15 23:45:26 +0100

    updated tests for re-estimation of standard errors

--- dml/tests/testthat/test-double_ml_plr.R
+++ dml/tests/testthat/test-double_ml_plr.R
@@ -8,6 +8,7 @@ lgr::get_logger("mlr3")$set_threshold("warn")
 
 test_cases = expand.grid(learner = c('regr.lm', 'regr.glmnet'),
                          dml_procedure = c('dml1', 'dml2'),
+                         se_reestimate = FALSE,
                          inf_model = c('IV-type', 'DML2018'),
                          i_setting = 1:(length(data_plr)),
                          stringsAsFactors = FALSE)
@@ -40,7 +41,9 @@ patrick::with_parameters_test_that("Unit tests for PLR:",
   double_mlplr_obj = DoubleMLPLR$new(n_folds = n_folds,
                                      ml_learners = learner_pars$mlmethod,
                                      params = learner_pars$params,
-                                     dml_procedure = dml_procedure, inf_model = inf_model)
+                                     dml_procedure = dml_procedure, 
+                                     se_reestimate = se_reestimate,
+                                     inf_model = inf_model)
   double_mlplr_obj$fit(data_plr[[i_setting]], y = "y", d = "d")
   theta_obj <- double_mlplr_obj$coef
   se_obj <- double_mlplr_obj$se
