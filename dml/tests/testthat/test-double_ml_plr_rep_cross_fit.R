890ec3ab0af437d07e8d4b23c5390e8cfd955ef3
malte <malte@scalable.capital>
2020-02-12 14:49:25 +0100

    make tests sensitive for split in est and boot part

--- dml/tests/testthat/test-double_ml_plr_rep_cross_fit.R
+++ dml/tests/testthat/test-double_ml_plr_rep_cross_fit.R
@@ -6,7 +6,7 @@ library('mlr3')
 
 lgr::get_logger("mlr3")$set_threshold("warn")
 
-test_cases = expand.grid(learner = c('regr.lm'), # removed 'regr.glmnet' for the moment as it causes issues for matching results between the oop and functional impl due to seeds etc
+test_cases = expand.grid(learner = c('regr.lm', 'regr.glmnet'), # removed 'regr.glmnet' for the moment as it causes issues for matching results between the oop and functional impl due to seeds etc
                          dml_procedure = c('dml1', 'dml2'),
                          se_reestimate = c(FALSE),
                          inf_model = c('IV-type', 'DML2018'),
@@ -22,6 +22,7 @@ patrick::with_parameters_test_that("Unit tests for PLR:",
   learner_pars_for_DML <- learner_pars
   learner_pars_for_DML$params$params_g = rep(list(learner_pars_for_DML$params$params_g), 1)
   learner_pars_for_DML$params$params_m = rep(list(learner_pars_for_DML$params$params_m), 1)
+  n_rep_boot = 498
   
   set.seed(i_setting)
   n_folds = 5
@@ -32,7 +33,7 @@ patrick::with_parameters_test_that("Unit tests for PLR:",
                  params = learner_pars_for_DML$params,
                  dml_procedure = dml_procedure, inf_model = inf_model,
                  se_type = inf_model,
-                 bootstrap = "none", nRep = 500)
+                 bootstrap = "normal", nRep = n_rep_boot)
   theta <- coef(plr_hat)
   se <- plr_hat$se
   
@@ -48,9 +49,14 @@ patrick::with_parameters_test_that("Unit tests for PLR:",
   theta_obj <- double_mlplr_obj$coef
   se_obj <- double_mlplr_obj$se
   
+  # bootstrap
+  double_mlplr_obj$bootstrap(method = 'normal',  n_rep = n_rep_boot)
+  boot_theta_obj = double_mlplr_obj$boot_coef
+  
   # at the moment the object result comes without a name
   expect_equal(theta, c(d=theta_obj), tolerance = 1e-8)
   expect_equal(se, c(d=se_obj), tolerance = 1e-8)
+  expect_equal(as.vector(plr_hat$boot_theta), as.vector(boot_theta_obj), tolerance = 1e-8)
 }
 )
 
