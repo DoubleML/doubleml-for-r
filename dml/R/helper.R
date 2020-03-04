f3134905018d87e3df0bc6562bd0e34c8e9abdb9
Philipp Bach <bach.philipp@outlook.com>
2020-03-04 15:42:33 +0100

    update extraction of probability predictions

--- dml/R/helper.R
+++ dml/R/helper.R
@@ -15,11 +15,10 @@ rearrange_prediction = function(prediction_list){
 }
 
 
-# TBD: implement for probability predictions
 extract_prob_prediction = function(obj_resampling) {
   f_hat <- as.data.table(obj_resampling$prediction())
   setorder(f_hat, 'row_id')
-  f_hat <- f_hat$prob.1
+  f_hat <- as.data.table(list("row_id" = f_hat$row_id, "response" = f_hat$prob.1))
   
   return(f_hat)
 }
