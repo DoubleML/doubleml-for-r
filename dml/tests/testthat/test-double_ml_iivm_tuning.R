58345b0f80773ad5f8a44ca3ca70c74555da0ff6
Philipp Bach <bach.philipp@outlook.com>
2020-03-06 10:06:52 +0100

    test for iivm tuning

---
+++ dml/tests/testthat/test-double_ml_iivm_tuning.R
@@ -0,0 +1,80 @@
+context("Unit tests for tuning of IIVM")
+
+library("mlr3learners")
+library("mlr3tuning")
+library("paradox")
+library('data.table')
+library('mlr3')
+lgr::get_logger("mlr3")$set_threshold("warn")
+
+# settings for parameter provision
+learner = c('rpart')
+
+learner_list = list("mlmethod_p" = learner, "mlmethod_mu" = learner, "mlmethod_m" = learner)
+
+tune_settings = list(n_folds_tune = 3,
+                      n_rep_tune = 1, 
+                      rsmp_tune = "cv", 
+                      measure_p = "classif.ce",
+                      measure_mu = "regr.mse", 
+                      measure_m = "classif.ce",
+                      terminator = mlr3tuning::term("evals", n_evals = 5), 
+                      algorithm = "grid_search",
+                      tuning_instance_p = NULL, 
+                      tuning_instance_mu = NULL,
+                      tuning_instance_m = NULL,
+                      tuner = "grid_search",
+                      resolution = 5)
+
+test_cases = expand.grid(learner = learner,
+                         dml_procedure = c('dml1', 'dml2'),
+                         se_reestimate = c(FALSE),
+                         inf_model = c('LATE'),
+                         i_setting = 1:(length(data_iivm)),
+                         n_rep_cross_fit = c(1, 3),
+                         stringsAsFactors = FALSE)
+test_cases['test_name'] = apply(test_cases, 1, paste, collapse="_")
+
+patrick::with_parameters_test_that("Unit tests for tuning of IIVM:",
+                                   .cases = test_cases, {
+  
+  learner_pars <- get_default_mlmethod_iivm(learner)
+  n_rep_boot = 498
+  n_folds = 2
+  
+  set.seed(i_setting)
+  double_mliivm_obj_tuned = DoubleMLIIVM$new(n_folds = n_folds,
+                                     ml_learners = learner_pars$mlmethod,
+                                     dml_procedure = dml_procedure, 
+                                     se_reestimate = se_reestimate, 
+                                     inf_model = inf_model, 
+                                     n_rep_cross_fit = n_rep_cross_fit)
+  
+  tune_ps = ParamSet$new(list(
+                          ParamDbl$new("cp", lower = 0.001, upper = 0.1),
+                          ParamInt$new("minsplit", lower = 1, upper = 10)))
+  
+  double_mliivm_obj_tuned$param_set$param_set_p = tune_ps
+  double_mliivm_obj_tuned$param_set$param_set_mu = tune_ps
+  double_mliivm_obj_tuned$param_set$param_set_m = tune_ps
+  double_mliivm_obj_tuned$tune_settings = tune_settings
+
+  double_mliivm_obj_tuned$tune(data_iivm[[i_setting]], y = "y", d = "d", z = "z")
+  
+  double_mliivm_obj_tuned$fit(data_iivm[[i_setting]], y = "y", d = "d", z = "z")
+  
+  theta_obj_tuned <- double_mliivm_obj_tuned$coef
+  se_obj_tuned <- double_mliivm_obj_tuned$se
+
+  # bootstrap
+  # double_mlirm_obj_tuned$bootstrap(method = 'normal',  n_rep = n_rep_boot)
+  # boot_theta_obj_tuned = double_mlirm_obj_tuned$boot_coef
+  
+  
+  # restrictions to test
+    # Functional (tbd) vs OOP implementation (handling randomness in param selection!?)
+  expect_is(theta_obj_tuned, "numeric")
+  expect_is(se_obj_tuned, "numeric")
+}
+)
+
