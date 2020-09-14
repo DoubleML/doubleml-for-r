7b971ac54630987345c7e458127d35b9a6c4bbe0
Philipp Bach <bach.philipp@outlook.com>
2020-09-14 14:34:59 +0200

    upd package name in tests

--- tests/testthat_regression_tests.R
+++ tests/testthat_regression_tests.R
@@ -1,10 +1,10 @@
 
 library("testthat")
 library("patrick")
-library("dml")
+library("DoubleML")
 library("mlr3learners")
 library("data.table")
 library("mlr3")
 
 
-testthat::test_check("dml")
+testthat::test_check("DoubleML")
