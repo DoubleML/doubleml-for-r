eb777d1b68bbeed476ad04b9e470d997693898ab
Philipp Bach <bach.philipp@outlook.com>
2020-09-16 18:34:15 +0200

    upd test for plr tuning

--- tests/testthat/test-double_ml_plr_tuning.R
+++ tests/testthat/test-double_ml_plr_tuning.R
@@ -13,23 +13,24 @@ learner = c('regr.rpart')
 
 learner_list = list("mlmethod_m" = learner, "mlmethod_g" = learner)
 
-tune_settings = list(n_folds_tune = 3,
+tune_settings = list(n_folds_tune = 2,
                       n_rep_tune = 1, 
                       rsmp_tune = "cv", 
                       measure_g = "regr.mse", 
                       measure_m = "regr.mse",
-                      terminator = mlr3tuning::trm("evals", n_evals = 5), 
+                      terminator = mlr3tuning::trm("evals", n_evals = 2), 
                       algorithm = "grid_search",
                       tuning_instance_g = NULL, 
                       tuning_instance_m = NULL,
                       tuner = "grid_search",
-                      resolution = 5)
+                      resolution = 2)
 
 test_cases = expand.grid(learner = learner,
                          dml_procedure = c('dml1', 'dml2'),
                          se_reestimate = c(FALSE),
                          score = c('IV-type', 'partialling out'),
                          n_rep_cross_fit = c(1, 3),
+                         tune_on_folds = c(FALSE, TRUE),
                          i_setting = 1:(length(data_plr)),
                          stringsAsFactors = FALSE)
 
@@ -65,21 +66,21 @@ patrick::with_parameters_test_that("Unit tests for tuning of PLR:",
 
   double_mlplr_obj_tuned = DoubleMLPLR$new(data_ml, 
                                      n_folds = n_folds,
-                                     ml_learners = learner_list,
+                                     ml_g = learner_list$mlmethod_g,
+                                     ml_m = learner_list$mlmethod_m,
                                      dml_procedure = dml_procedure, 
-                                     se_reestimate = se_reestimate,
                                      score = score, 
                                      n_rep_cross_fit = n_rep_cross_fit)
   
-  tune_ps = ParamSet$new(list(
-                          ParamDbl$new("cp", lower = 0.001, upper = 0.1),
-                          ParamInt$new("minsplit", lower = 1, upper = 10)))
+  param_grid = list(param_set_g = ParamSet$new(list(
+                                          ParamDbl$new("cp", lower = 0.01, upper = 0.02),
+                                          ParamInt$new("minsplit", lower = 1, upper = 2))),
+                    param_set_m = ParamSet$new(list(
+                                          ParamDbl$new("cp", lower = 0.01, upper = 0.02),
+                                          ParamInt$new("minsplit", lower = 1, upper = 2))))
+                  
+  double_mlplr_obj_tuned$tune(param_set = param_grid, tune_on_folds = tune_on_folds)
   
-  double_mlplr_obj_tuned$param_set$param_set_g = tune_ps
-  double_mlplr_obj_tuned$param_set$param_set_m = tune_ps
-  
-  double_mlplr_obj_tuned$tune_settings = tune_settings
-  double_mlplr_obj_tuned$tune()
   double_mlplr_obj_tuned$fit()
   
   theta_obj_tuned <- double_mlplr_obj_tuned$coef
@@ -96,15 +97,15 @@ patrick::with_parameters_test_that("Unit tests for tuning of PLR:",
     
   expect_is(theta_obj_tuned, "numeric")
   expect_is(se_obj_tuned, "numeric")
-  
-  data_ml$use_other_treat_as_covariate = FALSE
-  double_mlplr_obj_tuned$tune()
-  double_mlplr_obj_tuned$fit()
-  theta_obj_tuned <- double_mlplr_obj_tuned$coef
-  se_obj_tuned <- double_mlplr_obj_tuned$se
-  
-  expect_is(theta_obj_tuned, "numeric")
-  expect_is(se_obj_tuned, "numeric")
+  # 
+  # data_ml$use_other_treat_as_covariate = FALSE
+  # double_mlplr_obj_tuned$tune()
+  # double_mlplr_obj_tuned$fit()
+  # theta_obj_tuned <- double_mlplr_obj_tuned$coef
+  # se_obj_tuned <- double_mlplr_obj_tuned$se
+  # 
+  # expect_is(theta_obj_tuned, "numeric")
+  # expect_is(se_obj_tuned, "numeric")
   }
 )
 
