3ff172adfd2df9b9847c73beaa89ad20efa89714
Philipp Bach <bach.philipp@outlook.com>
2020-10-29 18:17:27 +0100

    document and reorganize DoubleMLIIVM

--- R/double_ml_iivm.R
+++ R/double_ml_iivm.R
@@ -1,13 +1,98 @@
-#' DoubleMLIIVM R6 class
+#' @title DoubleMLIIVM
 #' 
-#' @docType class
-#' @importFrom R6 R6Class
+#' @description
+#' Double machine learning for interactive IV regression models. 
+#' 
+#' @format [R6::R6Class] object inheriting from [DoubleML].
+#' 
+#' @details 
+#' Interactive IV regression (IIVM) models take the form \cr
+#' \eqn{Y = g_0(D,X) + \zeta},  \cr
+#' \eqn{Z = m_0(X) + V}, \cr 
+#' with \eqn{\mathbb{E}[\zeta|X,Z]=0} and \eqn{\mathbb{E}[V|X] = 0}. \eqn{Y} is the outcome variable, \eqn{D \in \{0,1\}} is the binary treatment variable and \eqn{Z \in \{0,1\}} is a binary instrumental variable. Consider the functions \eqn{g_0}, \eqn{r_0} and \eqn{m_0}, where \eqn{g_0} maps the support of \eqn{(Z,X)} to \eqn{\mathbb{R}} and \eqn{r_0} and \eqn{m_0}, respectively, map the support of \eqn{(Z,X)} and \eqn{X} to \eqn{(\epsilon, 1-\epsilon)} for some \eqn{\epsilon \in (1, 1/2)}, such that 
+#' \eqn{Y = g_0(D,X) + \zeta,} \cr
+#' \eqn{D = r_0(D,X) + U,} \cr
+#' \eqn{Z = m_0(X) + V,} \cr
+#' with \eqn{\mathbb{E}[\zeta|Z,X]=0}, \eqn{\mathbb{E}[U|Z,X]=0} and \eqn{\mathbb{E}[V|X]=0}. The target parameter of interest in this model is the local average treatment effect (LATE), \cr
+#' \eqn{\theta_0 = \frac{\mathbb{E}[g_0(1,X)] - \mathbb{E}[g_0(0,X)]}{\mathbb{E}[r(1,X)] - \mathbb{E}[r(0,X)]}.}
+#' 
+#' 
+#' @usage NULL
+#' 
+#' @examples
+#' library(DoubleML)
+#' library(mlr3)
+#' library(mlr3learners)
+#' library(data.table)
+#' set.seed(2)
+#' ml_g = "regr.ranger"
+#' ml_m = "classif.ranger"
+#' ml_r = "classif.ranger"
+#' obj_dml_data = make_iivm_data(theta = 1)
+#' dml_iivm_obj = DoubleMLIIVM$new(obj_dml_data, ml_g, ml_m, ml_r)
+#' lapply(dml_iivm_obj$params_names(), function(x) 
+#' 
+#'               dml_iivm_obj$set__ml_nuisance_params(treat_var = "d", learner = x,
+#'                                      params = list("num.trees" = 10, "max.depth" = 2)))
+#' dml_iivm_obj$fit()
+#' dml_iivm_obj$summary()
 #' @export
-
-DoubleMLIIVM <- R6Class("DoubleMLIIVM", inherit = DoubleML, public = list(
+DoubleMLIIVM <-R6:: R6Class("DoubleMLIIVM", inherit = DoubleML, public = list(
+  #' @field subgroups (named `list(2)`) \cr
+  #' Named `list(2)` with options to adapt to cases with and without the subgroups of always-takers and never-takes. The entry `always_takers`(`logical(1)`) speficies whether there are always takers in the sample. The entry `never_takers` (`logical(1)`) speficies whether there are never takers in the sample.
   subgroups = NULL, 
+  
+  #' @field trimming_rule (`character(1)`) \cr
+  #' A `character(1)` specifying the trimming approach. 
   trimming_rule = NULL, 
+  
+  #' @field trimming_threshold (`numeric(1)`) \cr
+  #' The threshold used for timming.
   trimming_threshold = NULL,
+  
+   #' @description 
+  #' Creates a new instance of this R6 class. 
+  #' 
+  #' @param data (`DoubleMLData`) \cr
+  #' The `DoubleMLData` object providing the data and specifying the variables of the causal model.
+  #' 
+  #' @param ml_g (`character(1)`) \cr
+  #' A `character(1)` specifying the name of a [mlr3 regression learner][mlr3::LearnerRegr] that is available in [mlr3](https://mlr3.mlr-org.com/index.html) or its extension packages [mlr3learners](https://mlr3learners.mlr-org.com/) or [mlr3extralearners](https://mlr3extralearners.mlr-org.com/), for example `"regr.cv_glmnet"`. \cr
+  #' `ml_g` refers to the nuisance function \eqn{g_0(Z,X) = \mathbb{E}[Y|X,Z]}.
+  #' 
+  #' @param ml_m (`character(1)`) \cr
+  #' A `character(1)` specifying the name of a [mlr3 classification learner][mlr3::LearnerClassif] that is available in [mlr3](https://mlr3.mlr-org.com/index.html) or its extension packages [mlr3learners](https://mlr3learners.mlr-org.com/) or [mlr3extralearners](https://mlr3extralearners.mlr-org.com/), for example `"classif.cv_glmnet"`. \cr
+  #' `ml_m` refers to the nuisance function \eqn{m_0(X) = \mathbb{E}[Z|X]}.
+  #' 
+  #' @param ml_r (`character(1)`) \cr
+  #' A `character(1)` specifying the name of a [mlr3 classification learner][mlr3::LearnerClassif] that is available in [mlr3](https://mlr3.mlr-org.com/index.html) or its extension packages [mlr3learners](https://mlr3learners.mlr-org.com/) or [mlr3extralearners](https://mlr3extralearners.mlr-org.com/), for example `"classif.cv_glmnet"`. \cr
+  #' `ml_r` refers to the nuisance function \eqn{r_0(Z,X) = \mathbb{E}[D|X,Z]}.
+  #' 
+  #' @param n_folds (`integer(1)`)\cr
+  #' Number of folds. Default is `5`. 
+  #' 
+  #' @param n_rep (`integer(1)`) \cr
+  #' Number of repetitions for the sample splitting. Default is `1`. 
+  #' 
+  #' @param score (`character(1)`) \cr
+  #' A `character(1)` (`"LATE"` is the only choice) specifying the score function. Default is `"ATE"`. 
+  #' 
+  #' @param subgroups (named `list(2)`) \cr
+  #' Named `list(2)` with options to adapt to cases with and without the subgroups of always-takers and never-takes. The entry `always_takers`(`logical(1)`) speficies whether there are always takers in the sample. The entry `never_takers` (`logical(1)`) speficies whether there are never takers in the sample. Default is `list(always_takers = TRUE, never_takers = TRUE)`.
+  #' 
+  #' @param trimming_rule (`character(1)`) \cr
+  #' A `character(1)` (`"truncate"` is the only choice) specifying the trimming approach. Default is `"truncate"`. 
+  #' @param trimming_threshold (`numeric(1)`) \cr
+  #' The threshold used for timming. Default is `1e-12`. 
+  #' 
+  #' @param dml_procedure (`character(1)`) \cr
+  #' A `character(1)` (`"dml1"` or `"dml2"`) specifying the double machine learning algorithm. Default is `"dml2"`. 
+  #' 
+  #' @param draw_sample_splitting (`logical(1)`) \cr
+  #' Indicates whether the sample splitting should be drawn during initialization of the object. Default is `TRUE`. 
+  #' 
+  #' @param apply_cross_fitting (`logical(1)`) \cr
+  #' Indicates whether cross-fitting should be applied. Default is `TRUE`.  
   initialize = function(data, 
                         ml_g, 
                         ml_m, 
@@ -68,7 +153,7 @@ private = list(
     if (!private$fold_specific_params) {
           
           for (i_nuis in self$params_names()){
-            if (is.null(private$get__params(i_nuis))) {
+            if (is.null(self$get__params(i_nuis))) {
               message(paste("Parameter of learner for nuisance part", i_nuis, "are not tuned, results might not be valid!"))
             }
           }
@@ -76,7 +161,7 @@ private = list(
       cond_smpls <- private$get_cond_smpls(smpls, self$data$data_model$z)
       
       # nuisance m
-      ml_m = initiate_prob_learner(self$learner$ml_m, private$get__params("ml_m"))                  
+      ml_m = initiate_prob_learner(self$learner$ml_m, self$get__params("ml_m"))                  
       resampling_m = mlr3::rsmp("custom")$instantiate(task_m,
                                                         smpls$train_ids,
                                                         smpls$test_ids)
@@ -84,14 +169,14 @@ private = list(
       m_hat = extract_prob_prediction(r_m)$prob.1
       
       # nuisance g
-      ml_g0 = initiate_learner(self$learner$ml_g, private$get__params("ml_g0"))
+      ml_g0 = initiate_learner(self$learner$ml_g, self$get__params("ml_g0"))
       resampling_g0 <- mlr3::rsmp("custom")$instantiate(task_g,
                                                          cond_smpls$train_ids_0,
                                                          smpls$test_ids)
       r_g0 <- mlr3::resample(task_g, ml_g0, resampling_g0, store_models = TRUE)
       g0_hat = extract_prediction(r_g0)$response
       
-      ml_g1 = initiate_learner(self$learner$ml_g, private$get__params("ml_g1"))
+      ml_g1 = initiate_learner(self$learner$ml_g, self$get__params("ml_g1"))
       resampling_g1 <- mlr3::rsmp("custom")$instantiate(task_g,
                                                          cond_smpls$train_ids_1,
                                                          smpls$test_ids)
@@ -106,7 +191,7 @@ private = list(
       if (self$subgroups$always_takers == FALSE){
         r0_hat <- rep(0, nrow(self$data$data_model))
       } else if (self$subgroups$always_takers == TRUE){
-        ml_r0 = initiate_prob_learner(self$learner$ml_r, private$get__params("ml_r0"))
+        ml_r0 = initiate_prob_learner(self$learner$ml_r, self$get__params("ml_r0"))
         resampling_r0 <- mlr3::rsmp("custom")$instantiate(task_r,
                                                           cond_smpls$train_ids_0,
                                                           smpls$test_ids)
@@ -117,7 +202,7 @@ private = list(
       if (self$subgroups$never_takers == FALSE){
         r1_hat <- rep(1, nrow(self$data$data_model))
       } else if (self$subgroups$never_takers == TRUE){
-        ml_r1 = initiate_prob_learner(self$learner$ml_r, private$get__params("ml_r1"))
+        ml_r1 = initiate_prob_learner(self$learner$ml_r, self$get__params("ml_r1"))
         resampling_r1 <- mlr3::rsmp("custom")$instantiate(task_r,
                                                            cond_smpls$train_ids_1,
                                                            smpls$test_ids)
@@ -129,7 +214,7 @@ private = list(
       cond_smpls <- private$get_cond_smpls(smpls, self$data$data_model$z)
       
       # nuisance m
-      ml_m = lapply(private$get__params("ml_m"), function(x) 
+      ml_m = lapply(self$get__params("ml_m"), function(x) 
                                                   initiate_prob_learner(self$learner$ml_m, x))
       resampling_m = initiate_resampling(task_m, smpls$train_ids, smpls$test_ids)
       r_m = resample_dml(task_m, ml_m, resampling_m, store_models = TRUE)
@@ -137,14 +222,14 @@ private = list(
       m_hat = rearrange_prob_prediction(m_hat, smpls$test_ids)
       
       # nuisance g
-      ml_g0 = lapply(private$get__params("ml_g0"), function(x) 
+      ml_g0 = lapply(self$get__params("ml_g0"), function(x) 
                                                     initiate_learner(self$learner$ml_g, x))
       resampling_g0 <- initiate_resampling(task_g, cond_smpls$train_ids_0, smpls$test_ids)
       r_g0 <- resample_dml(task_g, ml_g0, resampling_g0, store_models = TRUE)
       g0_hat <- lapply(r_g0, extract_prediction)
       g0_hat <- rearrange_prediction(g0_hat, smpls$test_ids)
 
-      ml_g1 = lapply(private$get__params("ml_g1"), function(x) 
+      ml_g1 = lapply(self$get__params("ml_g1"), function(x) 
                                                   initiate_learner(self$learner$ml_g, x))
       resampling_g1 <- initiate_resampling(task_g, cond_smpls$train_ids_1, smpls$test_ids)
       r_g1 <- resample_dml(task_g, ml_g1, resampling_g1, store_models = TRUE)
@@ -157,7 +242,7 @@ private = list(
       if (self$subgroups$always_takers == FALSE){
         r0_hat <- rep(0, nrow(self$data$data_model))
       } else if (self$subgroups$always_takers == TRUE){
-        ml_r0 = lapply(private$get__params("ml_r0"), function(x) 
+        ml_r0 = lapply(self$get__params("ml_r0"), function(x) 
                                                   initiate_prob_learner(self$learner$ml_r, x))
         resampling_r0 <- initiate_resampling(task_r, cond_smpls$train_ids_0, smpls$test_ids)
         r_r0 <- resample_dml(task_r, ml_r0, resampling_r0, store_models = TRUE)
@@ -167,7 +252,7 @@ private = list(
       if (self$subgroups$never_takers == FALSE){
         r1_hat <- rep(1, nrow(self$data$data_model))
       } else if (self$subgroups$never_takers == TRUE){
-        ml_r1 = lapply(private$get__params("ml_r1"), function(x) 
+        ml_r1 = lapply(self$get__params("ml_r1"), function(x) 
                                                   initiate_prob_learner(self$learner$ml_r, x))
         resampling_r1 <- initiate_resampling(task_r, cond_smpls$train_ids_1, smpls$test_ids)
         r_r1 <- resample_dml(task_r, ml_r1, resampling_r1, store_models = TRUE)
@@ -198,9 +283,9 @@ private = list(
                 psi_b = psi_b))
   },
  ml_nuisance_tuning = function(smpls, param_set, tune_on_folds, tune_settings, ...){
-   checkmate::check_class(param_set$param_set_g, "ParamSet")    
-   checkmate::check_class(param_set$param_set_m, "ParamSet")
-   checkmate::check_class(param_set$param_set_r, "ParamSet")
+   checkmate::check_class(param_set$ml_g, "ParamSet")    
+   checkmate::check_class(param_set$ml_m, "ParamSet")
+   checkmate::check_class(param_set$ml_r, "ParamSet")
 
    if (!tune_on_folds){
       data_tune_list = list(self$data$data_model)
@@ -213,31 +298,31 @@ private = list(
    } else {
      CV_tune = mlr3::rsmp(tune_settings$rsmp_tune, folds = tune_settings$n_folds_tune)
    }
-   if (any(class(tune_settings$measure$measure_m) == "Measure")) {
-        measure_m = tune_settings$measur$measure_m
+   if (any(class(tune_settings$measure$ml_m) == "Measure")) {
+        measure_m = tune_settings$measure$ml_m
       } else {
-          if (is.null(tune_settings$measure$measure_m)){
+          if (is.null(tune_settings$measure$ml_m)){
             measure_m = mlr3::default_measures("classif")[[1]]
           } else {
-            measure_m = mlr3::msr(tune_settings$measure$measure_m)
+            measure_m = mlr3::msr(tune_settings$measure$ml_m)
         }
    }
-   if (any(class(tune_settings$measure$measure_g) == "Measure")) {
-        measure_g = tune_settings$measure$measure_g
+   if (any(class(tune_settings$measure$ml_g) == "Measure")) {
+        measure_g = tune_settings$measure$ml_g
       } else {
-          if (is.null(tune_settings$measure$measure_g)){
+          if (is.null(tune_settings$measure$ml_g)){
             measure_g = mlr3::default_measures("regr")[[1]]
           } else {
-            measure_g = mlr3::msr(tune_settings$measure$measure_g)
+            measure_g = mlr3::msr(tune_settings$measure$ml_g)
         }
    }
-   if (any(class(tune_settings$measure$measure_r) == "Measure")) {
-        measure_r = tune_settings$measure$measure_r
+   if (any(class(tune_settings$measure$ml_r) == "Measure")) {
+        measure_r = tune_settings$measure$ml_r
       } else {
-          if (is.null(tune_settings$measure$measure_r)){
+          if (is.null(tune_settings$measure$ml_r)){
             measure_r= mlr3::default_measures("classif")[[1]]
           } else {
-            measure_r = mlr3::msr(tune_settings$measure$measure_r)
+            measure_r = mlr3::msr(tune_settings$measure$ml_r)
         }
       }
    
@@ -257,7 +342,7 @@ private = list(
                                           learner = ml_m,
                                           resampling = CV_tune,
                                           measure = measure_m,
-                                          search_space = param_set$param_set_m,
+                                          search_space = param_set$ml_m,
                                           terminator = terminator))
    tuning_result_m = lapply(tuning_instance_m, function(x) tune_instance(tuner, x))
    
@@ -269,7 +354,7 @@ private = list(
                                          learner = ml_g0,
                                          resampling = CV_tune,
                                          measure = measure_g,
-                                         search_space = param_set$param_set_g,
+                                         search_space = param_set$ml_g,
                                          terminator = terminator))
    tuning_result_g0 = lapply(tuning_instance_g0, function(x) tune_instance(tuner, x))
    
@@ -281,7 +366,7 @@ private = list(
                                          learner = ml_g1,
                                          resampling = CV_tune,
                                          measure = measure_g,
-                                         search_space = param_set$param_set_g,
+                                         search_space = param_set$ml_g,
                                          terminator = terminator))
    tuning_result_g1 = lapply(tuning_instance_g1, function(x) tune_instance(tuner, x))
    
@@ -295,7 +380,7 @@ private = list(
                                          learner = ml_r0,
                                          resampling = CV_tune,
                                          measure = measure_r,
-                                         search_space = param_set$param_set_r,
+                                         search_space = param_set$ml_r,
                                          terminator = terminator))
      tuning_result_r0 = lapply(tuning_instance_r0, function(x) tune_instance(tuner, x))
     } else {
@@ -312,7 +397,7 @@ private = list(
                                          learner = ml_r1,
                                          resampling = CV_tune,
                                          measure = measure_r,
-                                         search_space = param_set$param_set_r,
+                                         search_space = param_set$ml_r,
                                          terminator = terminator))
      tuning_result_r1 = lapply(tuning_instance_r1, function(x) tune_instance(tuner, x))
    } else {
