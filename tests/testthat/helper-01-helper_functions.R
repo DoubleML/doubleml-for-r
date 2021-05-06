739fbe9264691feaf9b90d3276662dc1d213af87
Malte S. Kurz <malte.s.kurz@gmail.com>
2021-05-06 10:13:50 +0200

    no need for docu at this place

--- tests/testthat/helper-01-helper_functions.R
+++ tests/testthat/helper-01-helper_functions.R
@@ -1,30 +1,8 @@
-#' Helper Functions
-#'
-#' Functions needed in package code.
-#'
-#' @param x some object.
-#' @name helpers
-NULL
-
-
-#' @rdname helpers
 extract_test_pred = function(x) {
   pred = x$data$response
   return(pred)
 }
 
-#' @param probs probability.
-#' @param digits number of digits.
-#' @rdname helpers
-# format.perc = function (probs, digits) {
-#   paste(format(100 * probs, trim = TRUE, scientific = FALSE, digits = digits),
-#         "%" ) }
-
-#' @param se_s Standard errors from repeated cross-fitting.
-#' @param coefficients Final estimators aggregated over all repetitions.
-#' @param theta_s Estimators from all repetitions.
-#' @inheritParams DML
-#' @rdname helpers
 se_repeated = function(se_s, coefficients, theta_s, aggreg_median) {
   if (aggreg_median) {
     se = sqrt(stats::median(se_s^2 + (theta_s - coefficients)^2))
