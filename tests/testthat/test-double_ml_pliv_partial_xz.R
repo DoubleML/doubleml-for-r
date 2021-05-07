7e602e5c08392f6ce4f532cd446d01036924676a
Malte S. Kurz <malte.s.kurz@gmail.com>
2021-05-07 13:33:13 +0200

    functional impl of PLIV.partialXZ with unit tests

--- tests/testthat/test-double_ml_pliv_partial_xz.R
+++ tests/testthat/test-double_ml_pliv_partial_xz.R
@@ -1,4 +1,4 @@
-context("Unit tests for PLIV.partialZ")
+context("Unit tests for PLIV.partialXZ")
 
 library("mlr3learners")
 
@@ -14,14 +14,14 @@ test_cases = expand.grid(
   stringsAsFactors = FALSE)
 test_cases["test_name"] = apply(test_cases, 1, paste, collapse = "_")
 
-patrick::with_parameters_test_that("Unit tests for PLIV.partialZ:",
+patrick::with_parameters_test_that("Unit tests for PLIV.partialXZ:",
   .cases = test_cases, {
     learner_pars = get_default_mlmethod_pliv(learner)
     n_rep_boot = 498
 
     set.seed(i_setting)
-    dim_z = 5
-    pliv_hat = dml_pliv_partial_z(data_pliv_partialZ[[i_setting]],
+    dim_z = 150
+    pliv_hat = dml_pliv_partial_xz(data_pliv_partialXZ[[i_setting]],
       y = "y", d = "d", z = paste0("Z", 1:dim_z),
       n_folds = 5, mlmethod = learner_pars$mlmethod,
       params = learner_pars$params,
@@ -30,8 +30,8 @@ patrick::with_parameters_test_that("Unit tests for PLIV.partialZ:",
     se = pliv_hat$se
     
     set.seed(i_setting)
-    boot_theta = bootstrap_pliv_partial_z(pliv_hat$thetas, pliv_hat$ses,
-                                 data_pliv_partialZ[[i_setting]],
+    boot_theta = bootstrap_pliv_partial_xz(pliv_hat$thetas, pliv_hat$ses,
+                                 data_pliv_partialXZ[[i_setting]],
                                  y = "y", d = "d", z = paste0("Z", 1:dim_z),
                                  n_folds = 5, smpls = pliv_hat$smpls,
                                  all_preds= pliv_hat$all_preds,
@@ -39,23 +39,33 @@ patrick::with_parameters_test_that("Unit tests for PLIV.partialZ:",
                                  bootstrap = "normal", n_rep_boot = n_rep_boot)$boot_coef
 
     set.seed(i_setting)
-    Xnames = names(data_pliv_partialZ[[i_setting]])[names(data_pliv_partialZ[[i_setting]]) %in% c("y", "d", paste0("Z", 1:dim_z)) == FALSE]
-    data_ml = double_ml_data_from_data_frame(data_pliv_partialZ[[i_setting]],
+    Xnames = names(data_pliv_partialXZ[[i_setting]])[names(data_pliv_partialXZ[[i_setting]]) %in% c("y", "d", paste0("Z", 1:dim_z)) == FALSE]
+    data_ml = double_ml_data_from_data_frame(data_pliv_partialXZ[[i_setting]],
       y_col = "y",
       d_cols = "d", x_cols = Xnames, z_cols = paste0("Z", 1:dim_z))
     
-    double_mlpliv_obj = DoubleMLPLIV.partialZ(data_ml,
-                                              ml_r = learner_pars$mlmethod$mlmethod_r,
-                                              n_folds = 5,
-                                              score = score,
-                                              dml_procedure = dml_procedure)
+    double_mlpliv_obj = DoubleML:::DoubleMLPLIV.partialXZ(data_ml,
+                                               ml_g = learner_pars$mlmethod$mlmethod_g,
+                                               ml_m = learner_pars$mlmethod$mlmethod_m,
+                                               ml_r = learner_pars$mlmethod$mlmethod_r,
+                                               n_folds = 5,
+                                               score = score,
+                                               dml_procedure = dml_procedure)
     
+    double_mlpliv_obj$set_ml_nuisance_params(
+      learner = "ml_g",
+      treat_var = "d",
+      params = learner_pars$params$params_g)
+    double_mlpliv_obj$set_ml_nuisance_params(
+      learner = "ml_m",
+      treat_var = "d",
+      params = learner_pars$params$params_m)
     double_mlpliv_obj$set_ml_nuisance_params(
       learner = "ml_r",
       treat_var = "d",
       params = learner_pars$params$params_r)
 
-    double_mlpliv_obj$fit()
+    double_mlpliv_obj$fit(store_predictions=TRUE)
     theta_obj = double_mlpliv_obj$coef
     se_obj = double_mlpliv_obj$se
 
