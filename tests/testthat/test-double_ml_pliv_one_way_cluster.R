5168f9d066120dc2c96f04cc7feb48724719f9f5
Malte S. Kurz <malte.s.kurz@gmail.com>
2021-07-27 17:00:31 +0200

    add unit test for one-way clustering

---
+++ tests/testthat/test-double_ml_pliv_one_way_cluster.R
@@ -0,0 +1,72 @@
+context("Unit tests for PLIV with one-way clustering")
+
+library("mlr3learners")
+
+lgr::get_logger("mlr3")$set_threshold("warn")
+
+on_cran = !identical(Sys.getenv("NOT_CRAN"), "true")
+if (on_cran) {
+  test_cases = expand.grid(
+    learner = "regr.glmnet",
+    dml_procedure = "dml1",
+    score = "partialling out",
+    stringsAsFactors = FALSE)
+} else {
+  test_cases = expand.grid(
+    learner = c("regr.lm", "regr.glmnet"),
+    dml_procedure = c("dml1", "dml2"),
+    score = "partialling out",
+    stringsAsFactors = FALSE)
+}
+test_cases["test_name"] = apply(test_cases, 1, paste, collapse = "_")
+
+set.seed(1234)
+N = 25  # number of observations (first dimension)
+M = 25  # number of observations (second dimension)
+dim_x = 100  # dimension of x
+data_one_way = make_pliv_multiway_cluster_CKMS2021(N, M, dim_x,
+                                                   omega_X = c(0.25, 0),
+                                                   omega_epsilon = c(0.25, 0),
+                                                   omega_v = c(0.25, 0),
+                                                   omega_V = c(0.25, 0))
+data_one_way$cluster_cols = 'cluster_var_i'
+
+patrick::with_parameters_test_that("Unit tests for PLIV with one-way clustering:",
+  .cases = test_cases, {
+    learner_pars = get_default_mlmethod_pliv(learner)
+    
+    set.seed(3141)
+    double_mlpliv_obj = DoubleMLPLIV$new(
+      data = data_one_way,
+      n_folds = 2,
+      ml_g = learner_pars$ml_g$clone(),
+      ml_m = learner_pars$ml_m$clone(),
+      ml_r = learner_pars$ml_r$clone(),
+      dml_procedure = dml_procedure,
+      score = score)
+    
+    set.seed(3141)
+    double_mlpliv_obj$fit()
+    theta_obj = double_mlpliv_obj$coef
+    #se_obj = double_mlpliv_obj$se
+    
+    set.seed(3141)
+    df = as.data.frame(data_one_way$data)
+    # need to drop variables as x is not explicitly set
+    df = df[ , !(names(df) %in% c('cluster_var_i', 'cluster_var_j'))]
+    pliv_hat = dml_pliv(df,
+      y = "Y", d = "D", z = "Z",
+      n_folds = 2,
+      ml_g = learner_pars$ml_g$clone(),
+      ml_m = learner_pars$ml_m$clone(),
+      ml_r = learner_pars$ml_r$clone(),
+      dml_procedure = dml_procedure, score = score,
+      smpls=double_mlpliv_obj$smpls)
+    theta = pliv_hat$coef
+    #se = pliv_hat$se
+
+    # at the moment the object result comes without a name
+    expect_equal(theta, theta_obj, tolerance = 1e-8)
+    #expect_equal(se, se_obj, tolerance = 1e-8)
+  }
+)
