b2fcc3da6ba28383491bcc678893aa00ec755633
Malte S. Kurz <malte.s.kurz@gmail.com>
2022-05-13 09:31:30 +0200

    apply styler

--- tests/testthat/test-double_ml_pliv_exception_handling.R
+++ tests/testthat/test-double_ml_pliv_exception_handling.R
@@ -8,17 +8,17 @@ lgr::get_logger("mlr3")$set_threshold("warn")
 
 test_that("Unit tests for deprecation warnings of PLIV", {
   set.seed(3141)
-  dml_data_pliv = make_pliv_CHS2015(n_obs=51, dim_z=1)
+  dml_data_pliv = make_pliv_CHS2015(n_obs = 51, dim_z = 1)
   ml_g = lrn("regr.ranger")
   ml_m = lrn("regr.ranger")
   ml_r = lrn("regr.ranger")
   msg = paste0("The argument ml_g was renamed to ml_l.")
   expect_warning(DoubleMLPLIV$new(dml_data_pliv,
-                                  ml_g = ml_g, ml_m = ml_m, ml_r = ml_r),
-    regexp = msg)
+    ml_g = ml_g, ml_m = ml_m, ml_r = ml_r),
+  regexp = msg)
 
   dml_obj = DoubleMLPLIV$new(dml_data_pliv,
-                             ml_l = ml_g, ml_m = ml_m, ml_r = ml_r)
+    ml_l = ml_g, ml_m = ml_m, ml_r = ml_r)
 
   msg = paste0("Learner ml_g was renamed to ml_l.")
   expect_warning(dml_obj$set_ml_nuisance_params(
