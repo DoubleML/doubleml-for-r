b96ac050265ea677be556452f387341f1387efdd
Malte S. Kurz <malte.s.kurz@gmail.com>
2022-06-13 08:43:42 +0200

    apply styler

--- tests/testthat/test-double_ml_pliv_partial_functional_initializer_IVtype.R
+++ tests/testthat/test-double_ml_pliv_partial_functional_initializer_IVtype.R
@@ -24,40 +24,40 @@ patrick::with_parameters_test_that("Unit tests for PLIV (partialX functional ini
     df = data_pliv$df
     Xnames = names(df)[names(df) %in% c("y", "d", "z", "z2") == FALSE]
     data_ml = double_ml_data_from_data_frame(df,
-                                             y_col = "y",
-                                             d_cols = "d", x_cols = Xnames, z_cols = "z")
-    
+      y_col = "y",
+      d_cols = "d", x_cols = Xnames, z_cols = "z")
+
     # Partial out X (default PLIV)
     set.seed(3141)
     double_mlpliv_obj = DoubleMLPLIV$new(data_ml,
-                                         n_folds = 5,
-                                         ml_l = learner_pars$ml_l$clone(),
-                                         ml_m = learner_pars$ml_m$clone(),
-                                         ml_r = learner_pars$ml_r$clone(),
-                                         ml_g = learner_pars$ml_g$clone(),
-                                         dml_procedure = dml_procedure,
-                                         score = score)
-    
+      n_folds = 5,
+      ml_l = learner_pars$ml_l$clone(),
+      ml_m = learner_pars$ml_m$clone(),
+      ml_r = learner_pars$ml_r$clone(),
+      ml_g = learner_pars$ml_g$clone(),
+      dml_procedure = dml_procedure,
+      score = score)
+
     double_mlpliv_obj$fit()
     theta_obj = double_mlpliv_obj$coef
     se_obj = double_mlpliv_obj$se
-    
+
     # Partial out X
     set.seed(3141)
     double_mlpliv_partX = DoubleMLPLIV.partialX(data_ml,
-                                                n_folds = 5,
-                                                ml_l = learner_pars$ml_l$clone(),
-                                                ml_m = learner_pars$ml_m$clone(),
-                                                ml_r = learner_pars$ml_r$clone(),
-                                                ml_g = learner_pars$ml_g$clone(),
-                                                dml_procedure = dml_procedure,
-                                                score = score)
-  
-  double_mlpliv_partX$fit()
-  theta_partX = double_mlpliv_partX$coef
-  se_partX = double_mlpliv_partX$se
-  
-  expect_equal(theta_partX, theta_obj, tolerance = 1e-8)
-  expect_equal(se_partX, se_obj, tolerance = 1e-8)
+      n_folds = 5,
+      ml_l = learner_pars$ml_l$clone(),
+      ml_m = learner_pars$ml_m$clone(),
+      ml_r = learner_pars$ml_r$clone(),
+      ml_g = learner_pars$ml_g$clone(),
+      dml_procedure = dml_procedure,
+      score = score)
+
+    double_mlpliv_partX$fit()
+    theta_partX = double_mlpliv_partX$coef
+    se_partX = double_mlpliv_partX$se
+
+    expect_equal(theta_partX, theta_obj, tolerance = 1e-8)
+    expect_equal(se_partX, se_obj, tolerance = 1e-8)
   }
 )
